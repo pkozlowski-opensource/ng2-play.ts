da93ead65744183ebbdb83e5d6b2f53da8403f9c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-10-03 14:18:35 +0200

    chore: remove dependency on traceur runtime

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var PATHS = {
         html: 'src/**/*.html'
     },
     lib: [
-        'node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
         'node_modules/angular2/bundles/angular2.min.js',
         'node_modules/systemjs/dist/system-csp-production.js'
     ],
